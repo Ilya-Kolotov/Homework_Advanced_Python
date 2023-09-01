dceadb28a5035d0f20ee40b281e0955d07f3a942
Ilya.Kolotov <kolotov.iljya@gmail.com>
2023-09-01 20:29:15 +0500

    Add Task1

--- HW_1/Task1.py
+++ HW_1/Task1.py
@@ -1,3 +1,15 @@
 """
 Вывести калькулятор как в тетради
 """
+
+for i in range(1, 11):
+    for j in range(2, 6):
+        multi = i * j
+        print(f'{j} x {i:2d} = {multi:2d} ', end='\t')
+    print()
+print()
+for i in range(1, 11):
+    for j in range(6, 10):
+        multi = i * j
+        print(f'{j} x {i:2d} = {multi:2d} ', end='\t')
+    print()
