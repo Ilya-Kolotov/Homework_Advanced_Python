ce97c1d32eb0dd5c7c1493bdc650e048b14072ae
Ilya.Kolotov <kolotov.iljya@gmail.com>
2023-09-22 19:45:23 +0500

    Add HW5 Task2

---
+++ HW_5/Task_2.py
@@ -0,0 +1,16 @@
+"""
+ Напишите функцию, которая принимает на вход строку —
+абсолютный путь до файла. Функция возвращает кортеж из трёх
+элементов: путь, имя файла, расширение файла.
+"""
+
+import os
+
+
+def parse_path(path):
+    filepath, file_extension = os.path.splitext(path)
+    dirname, filename = os.path.split(filepath)
+    return (dirname, filename, file_extension)
+
+
+print(parse_path(r"C:\Users\Admin\Desktop\Learn\Advanced_python\Homework\h.py"))
